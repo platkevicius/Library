0814d7f162df6a306a8b30f965bf76270ec49d3a
Louis Niederloehner <louis.niederloehner@unibw.de>
2022-05-20 11:59:27 +0200

    Added some links, smooth scrolling and disappearing buttons for authors

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
-import { Router } from '@angular/router';
-import { SearchService } from 'src/app/services/search.service';
-import {Authors} from "../../models/Authors";
+import {Router} from '@angular/router';
+import {SearchService} from 'src/app/services/search.service';
+import {Authors} from '../../models/Authors';
 
 @Component({
   selector: 'app-home',
@@ -11,37 +11,75 @@ import {Authors} from "../../models/Authors";
 export class HomeComponent implements OnInit {
 
   mockData: Authors[] = [
-    {name: "Louis Niederloehner", downloadCount: 49, publicationCount: 6},
-    {name: "Louis Niederloehner", downloadCount: 49, publicationCount: 6},
-    {name: "Louis Niederloehner", downloadCount: 49, publicationCount: 6},
-    {name: "Louis Niederloehner", downloadCount: 49, publicationCount: 6},
-    {name: "Louis Niederloehner", downloadCount: 49, publicationCount: 6},
-    {name: "Louis Niederloehner", downloadCount: 49, publicationCount: 6},
-    {name: "Louis Niederloehner", downloadCount: 49, publicationCount: 6},
-    {name: "Louis Niederloehner", downloadCount: 49, publicationCount: 6},
-    {name: "Louis Niederloehner", downloadCount: 49, publicationCount: 6},
-    {name: "Louis Niederloehner", downloadCount: 49, publicationCount: 6}
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6},
+    {name: 'Louis Niederloehner', downloadCount: 49, publicationCount: 6}
   ];
 
   @Input() authorLength: number = this.mockData.length;
 
-  constructor(private searchService: SearchService, private router: Router) { }
+  constructor(private searchService: SearchService, private router: Router) {
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   onSubmit(): void {
-    this.router.navigate(['/searchResult'])
+    this.router.navigate(['/searchResult']);
 
-    //TODO: add rest call for getting data
+    // TODO: add rest call for getting data
     this.searchService.searchByQuery('mathematics').subscribe(res => console.log(res));
   }
 
   clickScrollRight(): void {
-    document.getElementById('authors').scrollBy(500, 0);
+    document.getElementById('authors').scrollBy({
+      top: 0,
+      left: 500,
+      behavior: 'smooth'
+    });
+    this.checkButtonVisibility(500);
   }
 
   clickScrollLeft(): void {
-    document.getElementById('authors').scrollBy(-500, 0);
+    document.getElementById('authors').scrollBy({
+      top: 0,
+      left: -500,
+      behavior: 'smooth'
+    });
+    this.checkButtonVisibility(-500);
+  }
+
+  checkButtonVisibility(scrollingBy = 0): void {
+    const scrollElem = document.getElementById('authors');
+    const scrollDist = scrollElem.scrollLeft;
+    if (scrollDist + scrollingBy <= 0) {
+      document.getElementById('scroll-left').style.display = 'none';
+    } else {
+      document.getElementById('scroll-left').style.display = 'block';
+    }
+
+    if (scrollDist + scrollElem.offsetWidth + scrollingBy >= scrollElem.scrollWidth){
+      document.getElementById('scroll-right').style.display = 'none';
+    } else {
+      document.getElementById('scroll-right').style.display = 'block';
+    }
   }
 
 }
